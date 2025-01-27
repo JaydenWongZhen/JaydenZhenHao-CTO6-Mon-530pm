8e1ecf92340302226cc5adf47e5993ca263a9628
JaydenWongZhen <taymeister@gmail.com>
2025-01-27 09:47:40 +0000

    Jan 27, 2025, 9:47 AM

--- CT06_04/lesson4.py
+++ CT06_04/lesson4.py
@@ -1,11 +1,11 @@
 print("Hello from lesson 4")
-red=1
-blue_skibidi=2
-green_poweroftwo=3
+# red=1
+# blue_skibidi=2
+# green_poweroftwo=3
 
-poopee=3*red
-peepoo=5*blue_skibidi
-jakepaul=4*green_poweroftwo
+# poopee=3*red
+# peepoo=5*blue_skibidi
+# jakepaul=4*green_poweroftwo
 
 print(poopee+peepoo+jakepaul)
 
