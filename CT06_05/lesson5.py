0607526340a48f9a8b956688fbb24a1906256bfa
JaydenWongZhen <taymeister@gmail.com>
2025-02-03 10:20:45 +0000

    Feb 3, 2025, 10:20 AM

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -7,6 +7,6 @@ bday_message=input("Enter extra message you want to send here ")
 
 print("Happy " + str(bday_age) + "th birthday " , bday_name , "! " , bday_message)
 
-import random
-rand=random.randint(1,50)
-print(str(rand))
+# import random
+# rand=random.randint(1,50)
+# print(str(rand))
