a57ef5e992c39db127e461f3322b9b12ed0e1c84
JaydenWongZhen <taymeister@gmail.com>
2025-02-03 10:18:53 +0000

    Feb 3, 2025, 10:18 AM

--- CT06_04/lesson4.py
+++ CT06_04/lesson4.py
@@ -10,5 +10,70 @@ print("Hello from lesson 4")
 # print(poopee+peepoo+jakepaul)
 
 
-reply=input("WHAT IS YOUR nAME")
-print("GREETINGS PERSON " + reply)
+# reply=input("WHAT IS YOUR nAME")
+# print("GREETINGS " + reply)
+
+# name=input("What is your name? ")
+# print("Hello, "+name)
+# favoritecol=input("What is your favorite color?")
+# age=input("What is your age? ")
+# print("Your favorite color is "+favoritecol)
+# print("You are"+age+"years old")
+
+
+# reply=69
+# counter=69
+# print(reply*counter)
+
+
+# greeting=input("Hello! What is your name? ")
+# print("Hello, "+greeting+"!")
+
+# hobby=input("What is your favorite hobby? ")
+# print("I like "+hobby+" too!")
+
+# vacation=input("what is your DREAM vacation? ")
+# print("I also would love to visit "+vacation+" too!")
+
+mep=input("hhwhyhdh whats yo age ")
+mep = int(mep)
+mrbreast=2025.0523
+print("you will dead at "+str(mep)+ " years old 3 hours from now")
+
+
+
+
+
+
+
+## Task 4: Variable Reassignment Basics
+
+# Assign the string "Hello" to a variable 'message'. 
+# Then reassign 'message' to the integer "10" and print it.
+
+message = "hello"
+message = 10
+print(message)
+
+
+# ---------------------------------------------------------------
+
+## Task 5: Printing Full Name
+
+# Create 2 variables, 'firstName' and 'lastName', assigning your
+# first and last names to them. Then print them on the same line
+# with a space between them.
+firstnamewhichis = "Jayden Wong"
+lastnamewhichis = "Ligma Balls"
+print(firstnamewhichis + " " + lastnamewhichis)
+# ---------------------------------------------------------------
+
+## Task 6: Age and Name Concatenation
+
+# Assign a name to the variable "user_name"
+# Assign an integer to the variable "user_age"
+# Use type conversion and string concatenation to print out
+# "[user_name] is [user_age] years old."
+username=input("what name")
+userage=input("age?")
+userage = int(userage)
--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -1,1 +1,12 @@
 print("Hello from lesson 5")
+
+print("BIRTHDAY_CONSOLE ACTIVATED")
+bday_name=input("Enter name here ")
+bday_age=input("Enter age you are turning this year here ")
+bday_message=input("Enter extra message you want to send here ")
+
+print("Happy " + str(bday_age) + "th birthday " , bday_name , "! " , bday_message)
+
+import random
+rand=random.randint(1,50)
+print(str(rand))
