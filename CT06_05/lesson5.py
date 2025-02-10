7935172a44cb8294f8e9cd5ab190fd9cded3f638
JaydenWongZhen <taymeister@gmail.com>
2025-02-10 09:33:57 +0000

    Feb 10, 2025, 9:33 AM

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -1,12 +1,31 @@
 print("Hello from lesson 5")
 
-print("BIRTHDAY_CONSOLE ACTIVATED")
-bday_name=input("Enter name here ")
-bday_age=input("Enter age you are turning this year here ")
-bday_message=input("Enter extra message you want to send here ")
+# print("BIRTHDAY_CONSOLE ACTIVATED")
+# bday_name=input("Enter name here ")
+# bday_age=input("Enter age you are turning this year here ")
+# bday_message=input("Enter extra message you want to send here ")
 
-print("Happy " + str(bday_age) + "th birthday " , bday_name , "! " , bday_message)
+# print("Happy " + str(bday_age) + "th birthday " , bday_name , "! " , bday_message)
 
-# import random
-# rand=random.randint(1,50)
-# print(str(rand))
+import random
+rand=random.randint(1,50)
+print(str(rand))
+
+# for count in range(500):
+#     print("I will not ship bill nye x bill cipher")
+
+# for abc in "I EAT PEE!":
+#     print(abc)
+
+dave=input("wht is your name ")
+for d in dave:
+    print("give me an " + d)
+
+print("what have we got?")
+print("we got " + dave)
+
+# for le in range(100):
+#     print("i love chickenen ricicice")
+for ra in range(100):
+    print("i love caik")
+    print("GIVE ME MORE")
