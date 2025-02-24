5ba01a1d1033bcaea57ab7ce6d7927c6c7acd78d
JaydenWongZhen <taymeister@gmail.com>
2025-02-24 10:28:51 +0000

    Feb 24, 2025, 10:28 AM

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -22,7 +22,7 @@ print("Hello from lesson 7")
 # for b in range(10,0,-1):
 #     print(b)
 
-#### i accidentally did 5 first u
+#### i accidentally did 5 first but 4 is literally the same js
 naem=input("what is your name ")
 rep=int(input("how many time to repeat "))
 
