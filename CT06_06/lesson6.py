4b305a81bf8e0ab7b0ce22bed8032e82908b63a0
JaydenWongZhen <taymeister@gmail.com>
2025-02-24 09:32:41 +0000

    Feb 24, 2025, 9:32 AM

--- CT06_06/lesson6.py
+++ CT06_06/lesson6.py
@@ -1,1 +1,62 @@
 print("Hello from lesson 6")
+
+# numbchucks = 0
+
+# students=int(input("Give amount of students. "))
+# for stude in range(students):
+#     studntmarks=int(input("Amount of Student's marks here. "))
+#     numbchucks=numbchucks+studntmarks
+# qutotit=numbchucks/students
+# print("Average score: " + str(qutotit))
+
+# for i in range(3):
+#     print("Hello, World!")
+
+# for i in range(5):
+#     print(i)
+
+# print("hello world")
+
+# djdjdjjjdjjhcjfghufhrgrkgh=5
+
+# print("hi world")                                     
+
+# # **Task 2a**:
+# age=10
+# print(age)
+
+# # **Task 2b**:
+# name = "Alice"
+# print(name)
+
+# # **Task 2c**:
+# x=21
+# print(x)
+
+# # **Task 2d**:
+# print("Hello, World!")
+
+
+import random
+ra=random.randint(100,999)
+print(str(ra))
+if ra >= 888:
+    print("YOU WON!!!")
+
+# age=25
+# print(age + 1)
+
+# numberm=10
+# print(numberm-5)
+
+# print("Repeat " * 3)
+
+# yearn=2025
+# print("the year is " + str(yearn))
+
+# x=10
+# y=x/2
+
+# wah=5
+# for i in range(wah):
+#     print(i)
