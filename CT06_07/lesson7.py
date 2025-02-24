62c7f84d77e609e5ee7083538c7f5a36a4fbae12
JaydenWongZhen <taymeister@gmail.com>
2025-02-24 10:51:42 +0000

    Feb 24, 2025, 10:51 AM

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -33,7 +33,7 @@ print("Hello from lesson 7")
 totalsum=0
 eminem= "what is numbar #"
 for rblyx in range(5):
-    overwritten=int(input(eminem))
+    overwritten=int(input(eminem+str(rblyx)))
     totalsum=overwritten+totalsum
 
 print("your sum is " + str(totalsum))
