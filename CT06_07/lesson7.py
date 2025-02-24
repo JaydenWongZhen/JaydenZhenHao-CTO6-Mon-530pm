63e099d84f1747ca7af0877efe16bdfb98119cbe
JaydenWongZhen <taymeister@gmail.com>
2025-02-24 10:33:21 +0000

    Feb 24, 2025, 10:33 AM

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -23,17 +23,17 @@ print("Hello from lesson 7")
 #     print(b)
 
 #### i accidentally did 5 first but 4 is literally the same just withut the greeting string
-naem=input("what is your name ")
-rep=int(input("how many time to repeat "))
+# naem=input("what is your name ")
+# rep=int(input("how many time to repeat "))
 
-for roblox in range(rep):
-    print("hi and nice to meet you " + naem)
+# for roblox in range(rep):
+#     print("hi and nice to meet you " + naem)
 
 
-totalsum=0
+# totalsum=0
 
-for rblyx in range(5):
-    overwritten=int(input("tell me a number "))
-    totalsum=overwritten+totalsum
+# for rblyx in range(5):
+#     overwritten=int(input("tell me a number "))
+#     totalsum=overwritten+totalsum
 
-print("your sum is " + str(totalsum))
+# print("your sum is " + str(totalsum))
