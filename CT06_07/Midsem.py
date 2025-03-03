3fa8f0908be1a85b566065b3e1bcb5ed74d98202
JaydenWongZhen <taymeister@gmail.com>
2025-03-03 10:51:03 +0000

    Mar 3, 2025, 10:51 AM

---
+++ CT06_07/Midsem.py
@@ -0,0 +1,16 @@
+# n=input("What is your name? ")
+# print("Nice to meet you, " + n )
+
+#This is a customizable greeter where you can customize the name
+
+
+# start=int(input("What is the starting number? "))
+# end=int(input("What is the ending number? "))
+# increment=int(input("What would you like to count by? "))
+
+# # This is for asking for the intervals such as start,end and increment.
+
+# for data in range(start,end,increment):
+#     print(data)
+
+# # This is for counting the number.
