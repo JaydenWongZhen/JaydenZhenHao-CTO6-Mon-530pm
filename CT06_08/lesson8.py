682b11f528c247c9ce780797b6e5ecde583569ae
JaydenWongZhen <taymeister@gmail.com>
2025-03-10 09:40:18 +0000

    Mar 10, 2025, 9:40 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -7,7 +7,4 @@ for c in range(5):
     num = int(input("Give me a number "))
     pd = pd*num
 
-if pd >= 1000000000000:
-    pd="beyond my feeble brain to comprehend"
-
 print("final res: " + str(pd))
