90acae7eff123c8bbbff46716c41f9b36024f1fc
JaydenWongZhen <taymeister@gmail.com>
2025-03-17 10:54:28 +0000

    Mar 17, 2025, 10:54 AM

--- CT06_09/lesson9.py
+++ CT06_09/lesson9.py
@@ -1,8 +1,9 @@
-print("Hello from lesson 9")
+# print("Hello from lesson 9")
 
-print("Hello, welcome to the libary.")
-days=int(input("How many days have you borrowed this book? "))
-if days >= 25:
-    print("Remember to return your book, this book is due in 5 days!")
-else:
-    print("Alright, you're clear! Have a nice day!")
+# print("Hello, welcome to the libary.")
+# days=int(input("How many days have you borrowed this book? "))
+# if days >= 25:
+#     print("Remember to return your book, this book is due in 5 days!")
+# else:
+#     print("Alright, you're clear! Have a nice day!")
+
