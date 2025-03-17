634f394b6348c6c25aa4302890747657a50e87ae
JaydenWongZhen <taymeister@gmail.com>
2025-03-17 10:51:42 +0000

    Mar 17, 2025, 10:51 AM

--- CT06_09/lesson9.py
+++ CT06_09/lesson9.py
@@ -3,4 +3,4 @@ print("Hello from lesson 9")
 print("Hello, welcome to the libary.")
 days=int(input("How many days have you borrowed this book? "))
 if days >= 25:
-    print()
+    print("Remember to ")
