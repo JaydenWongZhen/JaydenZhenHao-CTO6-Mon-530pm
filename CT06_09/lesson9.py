c3e462fa1ce6dcdbc77026c7ee40bc7850c3fad3
JaydenWongZhen <taymeister@gmail.com>
2025-04-07 09:33:07 +0000

    Apr 7, 2025, 9:33 AM

--- CT06_09/lesson9.py
+++ CT06_09/lesson9.py
@@ -7,6 +7,39 @@
 # else:
 #     print("Alright, you're clear! Have a nice day!")
 
-import random
-answer=random.randint(1,50)
-guess=int(input("Guess "))
+# import random
+# answer=random.randint(1,50)
+# guess=int(input("Guess "))
+# if guess == answer:
+#     print("conguratulation")
+# else:
+#     print("try agin")
+
+# aplle=60
+# orangutan=90
+
+# numap=int(input("how much aplle you buy "))
+# numor=int(input("how much orangutan you buy "))
+
+# if numap*aplle >= aplle*5:
+#     priceapple=numap*aplle*0.9
+# else:
+#     priceapple=numap*aplle
+
+# if numor*orangutan >= orangutan*5:
+#     priceorangutan=numor*orangutan*0.9
+# else:
+#     priceorangutan=numor*orangutan
+
+# totalrpice=(priceorangutan+priceapple)/100
+# print("you have to pay $" + str(totalrpice))
+
+A=100
+numA=int(input("how much apples you wanna buy? ")) #THIS IS AN INPUT
+
+if numA*A >= A*10: #THIS IS A DECISION
+    print("here you go a deal, 10 for the price of 9 or 10% discount") #THIS IS AN OUTPUT
+    TotA=numA*A*0.9/100
+else: #ALSO A DECISION
+    TotA=numA*A/100
+print("please insert cash or card to pay $" + str(TotA)) #ALSO AN OUTPUT
--- CT06_10/lesson10.py
+++ CT06_10/lesson10.py
@@ -1,1 +1,22 @@
 print("Hello from lesson 10")
+
+# bbobb=int(input("tell me a negatie or postive num "))
+# if bbobb >= 0:
+#     print("my calculations show me that this is... Positive")
+# else:
+#     print("my calculations show me that this is... Negative")
+
+# import random
+# ehrurehr = random.randint(1,10)
+# frgrgh=int(input("give me THE number "))
+# if frgrgh == ehrurehr:
+#     print("good job.")
+# else:
+#     print("try again.")
+
+# password="wongzhenhao_aagu003"
+# login=input("Insert password | ")
+# if login == password:
+#     print("Correct! Ascess Granted.")
+# else:
+#     print("Password Incorrect. Try again.")
