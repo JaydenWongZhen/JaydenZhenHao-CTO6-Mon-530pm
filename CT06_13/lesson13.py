a78bfb0cc6621e42beb45bf3eaada99a48a3407c
JaydenWongZhen <taymeister@gmail.com>
2025-04-21 10:49:12 +0000

    Apr 21, 2025, 10:49 AM

--- CT06_13/lesson13.py
+++ CT06_13/lesson13.py
@@ -3,4 +3,4 @@ print("IM TIRED CAN I HAVE A BREAK PLEASE")
 balance=1000
 print("Welcome to the ATM.")
 print("Choose the 4 following. | Withdraw || Deposit || Check Balance ||")
-re
+reply = input()
