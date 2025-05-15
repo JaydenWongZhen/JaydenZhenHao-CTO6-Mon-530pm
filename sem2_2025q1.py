035954a2f8c9c9edfc5d4fda9e0168321421ff4c
JaydenWongZhen <taymeister@gmail.com>
2025-05-15 06:23:55 +0000

    May 15, 2025, 6:23 AM

--- sem2_2025q1.py
+++ sem2_2025q1.py
@@ -7,7 +7,7 @@
 # Example: 10, 20, 30 ..... 180, 190, 200.
 # Note that the numbers do not need to be printed in one line.
 # Write your code here
-ra = range9
+ra = range(9,200,10)
 while ra <= 200:
     print()
 
