149a21c705898db905b19559402bbd687b979835
JaydenWongZhen <taymeister@gmail.com>
2025-05-19 10:36:49 +0000

    May 19, 2025, 10:36 AM

--- CT06_14/lesson14.py
+++ CT06_14/lesson14.py
@@ -50,6 +50,6 @@ for count in range(15):
     randy=random.randint(-200,200)
 
     abert.goto(randx,randy)
-    abert.write(str(randomX) + "," + )
+    abert.write(str(randX) + "," + str(randy))
 
 scren.mainloop()
